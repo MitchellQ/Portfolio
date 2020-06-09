a626b1d032301801c612ada942f37013c17f208b
MitchellQ <mitchell.quarrie@outlook.com>
2020-06-10 10:56:41 +1200

    Update Landing.js

--- src/javascript/components/Landing.js
+++ src/javascript/components/Landing.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { sanitiseHtml } from '../functions';
 
 export default class Landing extends Component {
     constructor(props) {
@@ -10,6 +11,8 @@ export default class Landing extends Component {
     render() {
         const { greeting, message } = this.state;
 
+        let message_html = sanitiseHtml(message).replace(/\n/g, '</p><p>');
+
         return (
             <div className="site-blocks-cover overlay aos-init aos-animate" style={{backgroundImage: 'url(dist/images/landing.jpg)'}} data-aos="fade" id="home-section">
 
@@ -17,7 +20,7 @@ export default class Landing extends Component {
                     <div className="row align-items-center justify-content-center">
                         <div className="col-md-8 mt-lg-5 text-center">
                             <h1 className="text-uppercase aos-init aos-animate" data-aos="fade-up">{greeting}</h1>
-                            <p className="mb-5 desc aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">{message}</p>
+                            <p className="mb-5 desc aos-init aos-animate" data-aos="fade-up" data-aos-delay="100" dangerouslySetInnerHTML={{__html: message_html}}/>
                             <div data-aos="fade-up" data-aos-delay="100" className="aos-init aos-animate">
                                 <a href="#contact-section" className="btn smoothscroll btn-primary mr-2 mb-2">Get In Touch</a>
                             </div>
