18a2ca8558b0d68fed593960ed041d96137cff43
MitchellQ <mitchell.quarrie@outlook.com>
2020-06-09 15:52:56 +1200

    Base styles and js added

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -3,6 +3,24 @@
  * @repo https://github.com/mitchellq/mitchellq.github.io
  */
 
- // Boilerplate SCSS
+/* Boilerplate JavaScript
+ ------------------------------ */
+import 'jquery-ui';
+import 'popper.js'
+import 'bootstrap';
 
- // React
+import 'jquery.sticky';
+import 'jquery.easing';
+import 'jquery-countdown';
+import 'aos';
+
+import './vendor/owl.carousel.min';
+import './vendor/jquery.fancybox.min';
+import './vendor/isotope.pkgd.min';
+
+import './main';
+
+
+
+/* React Javascript
+ ------------------------------ */
