371a3f39cfb2ef3406413d736ceaf62ae016809b
MitchellQ <mitchell.quarrie@outlook.com>
2020-06-23 11:59:39 +1200

    Initial Routing config

--- src/javascript/Wrapper.js
+++ src/javascript/Wrapper.js
@@ -13,6 +13,8 @@ import Testimonials from './Testimonials';
 import Contact from './Contact';
 import Footer from './Footer';
 
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+
 export default class Wrapper extends Component {
     constructor(props) {
         super(props);
@@ -24,19 +26,32 @@ export default class Wrapper extends Component {
         const { testimonialsVisible, contactFormVisible } = this.state;
 
         return (
+            <BrowserRouter>
             <ErrorBoundary>
-                <Spinner />            
+                <Spinner />                
                 <div className="site-wrap">
                     <SiteNav />
-                    <Landing />              
+                    <Switch>
+                        <Route path="/">
+                            <h1>Test</h1>
+                        </Route>
+
+                        <Route path="/sam" component={Project}>
+                              
+                        </Route>
+
+                    </Switch>
+
+                    {/* <Landing />              
                     <Project />
                     <About />
                     <FeaturedProjects />
                     {testimonialsVisible ? <Testimonials /> : null}
-                    {contactFormVisible ? <Contact /> : null}
+                    {contactFormVisible ? <Contact /> : null} */}
                     <Footer />
                 </div>
             </ErrorBoundary>
+            </BrowserRouter>
         );
     }
 }
