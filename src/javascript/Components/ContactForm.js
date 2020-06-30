387d54ca6f992cbf5359a1c18cc803e79e7221ed
MitchellQ <mitchell.quarrie@outlook.com>
2020-07-01 10:55:02 +1200

    Created Contact Form component and removed all relevant code from contact.js and into new file

---
+++ src/javascript/Components/ContactForm.js
@@ -0,0 +1,183 @@
+import React from 'react';
+import { Input, TextArea, FormGroup } from './FormControls';
+import Captcha from './Captchpa';
+
+/** Import react-toastify to provide fancy notifications */
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export default class ContactForm extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: {
+                CaptchaCode: "",
+                From: {
+                    Name: "",
+                    Email: ""
+                },
+                Subject: "",
+                Message: ""
+            }
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.onVerified = this.onVerified.bind(this);
+    }
+
+    componentDidMount() {
+        let apiUrl = "https://localhost:44351/"
+
+        $.ajax({
+            method: 'get',
+            url: apiUrl,
+            crossDomain: true,
+            success: ((res) => {
+                this.setState({
+                    apiUrl,
+                    reCaptchaKey: res.reCaptchaKey
+                });
+            })
+        });
+    }
+
+
+    onVerified(code) {
+        let data = this.state.data;
+        data['CaptchaCode'] = code;
+        this.setState({ data });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        // Don't process the request if a reCAPTCHA code is required and missing
+        if (!!this.state.reCaptchaKey && !this.state.data.CaptchaCode) return;
+
+        let data = this.state.data;
+        data['Subject'] = "New contact form enquiry: " + data['Subject'];
+        console.log(data)
+        $.ajax({
+            type: 'post',
+            url: this.state.apiUrl,
+            dataType: "json",
+            crossDomain: true,
+            data,
+            success: ((msg) => toast.success(msg)),
+            error: ((err) => toast.error(err.responseText || err.responseJSON))
+        });
+    }
+
+    handleChange(key, val) {
+        let data = this.state.data;
+        data[key] = val
+        this.setState({
+            data
+        });
+    }
+
+    render() {
+        const { From, Message } = this.state.data;
+        const { errors, success } = this.state;
+
+        return (            
+            this.state.apiUrl && this.state.reCaptchaKey ? (
+                <div className="row">
+                    <div className="col-md-12 mb-5">
+                        <form onSubmit={this.handleSubmit} className="px-5 py-3 bg-white">
+                            
+                            <div className="row">
+                                <FormGroup className="form-group col-12 mb-3 mb-md-0">
+                                    <Input type="name" placeHolder="Name" required
+                                        autoComplete="name" value={this.state.email}
+                                        onChange={(val) => this.handleChange('From', {Name: val, Email: From.Email})}
+                                    />
+    
+                                    <Messages errors={errors ? errors.Name : null} type="error" />
+                                </FormGroup>
+    
+                                <FormGroup className="form-group col-12 mb-3 mb-md-0">
+                                    <Input type="email" placeHolder="Email" required
+                                        autoComplete="email" value={this.state.Email}
+                                        onChange={(val) => this.handleChange('From', {Name: From.Name, Email: val})}
+                                    />
+    
+                                    <Messages errors={errors ? errors.Email : null} type="error" />
+                                </FormGroup>
+    
+                                <FormGroup className="form-group col-12 mb-3 mb-md-0">
+                                    <Input type="text" placeHolder="Subject"
+                                        value={this.state.subject}
+                                        onChange={this.handleChange.bind(this, 'Subject')}
+                                    />
+                                </FormGroup>
+    
+                                <FormGroup className="form-group col-12">
+                                    <TextArea placeHolder="Write your notes or questions here..." required
+                                        value={this.state.message} cols="30" rows="7"
+                                        onChange={this.handleChange.bind(this, 'Message')}
+                                    />
+    
+                                    <Messages errors={errors ? errors.Message : null} type="error" />
+                                </FormGroup>
+    
+                                <Captcha onRef={reCaptcha => (this.reCaptcha = reCaptcha)} theme={'light'} size={'normal'}
+                                    sitekey={this.state.reCaptchaKey} requireReady={false} onVerified={this.onVerified}
+                                />
+    
+                                <div className="col-md-12">
+                                    <Button className="btn btn-primary btn-md text-white mt-3" disabled={!this.state.data.CaptchaCode && !!this.state.reCaptchaKey} pending={false}>
+                                        <i className="fas fa-envelope mr-2" /> Send Email
+                                    </Button>
+                                </div>
+    
+                                {/* Toast container */}
+                                <ToastContainer
+                                    position="top-right"
+                                    autoClose={10 * 1000}
+                                    hideProgressBar={true}
+                                    closeOnClick={true}
+                                    closeButton={true}
+                                    pauseOnHover
+                                />
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            ): null           
+        );
+    }
+}
+
+class Messages extends React.Component {
+    render() {
+
+        let color = this.props.type == "error" ? "danger" : "success";
+
+        return !this.props.errors ? null : (
+            <React.Fragment>
+                {
+                    this.props.errors.map((err, key) => {
+                        return <label className={`text-${color}`} key={key}>{err}</label>
+                    })
+                }
+            </React.Fragment>
+        )
+    }
+}
+
+class Button extends React.Component {
+    render() {
+        const { id, children, className, disabled, pending, readOnly } = this.props;
+
+        return <button id={id || null} className={className} disabled={!!disabled || !!pending} readOnly={!!readOnly}>
+            { !pending ? children : (
+            <React.Fragment>
+                <span class="sr-only">Loading...</span>
+                <i className="fas fa-spinner fa-spin"/>
+            </React.Fragment>
+            )}
+        </button>
+    }
+}
--- src/javascript/Contact.js
+++ src/javascript/Contact.js
@@ -1,69 +1,10 @@
 import React from 'react';
-import { Input, TextArea, FormGroup } from './Components/FormControls';
-import Captcha from './Components/Captchpa';
+import ContactForm from './Components/ContactForm';
 
 export default class Contact extends React.Component {
-    constructor(props) {
-        super(props);
 
-        this.state = {
-            // firstName: '',
-            // lastName: '',
-            // email: '',
-            // subject: '',
-            // message: '',
-
-            data: {
-                CaptchaCode: "",
-                From: {
-                    Name: "",
-                    Email: ""
-                },
-                Subject: "",
-                Message: ""
-            }
-        }
-
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    componentDidMount() {
-        let apiUrl = "https://localhost:44351/"
-
-        $.ajax({
-            method: 'get',
-            url: apiUrl,
-            crossDomain: true,
-            success: ((res) => {
-                this.setState({
-                    apiUrl,
-                    reCaptchaKey: res.reCaptchaKey
-                });
-            })
-        });
-    }
-
-    onVerified(code) {
-        let data = this.state.data;
-        data['CaptchaCode'] = code;
-        this.setState({data});
-    }
-
-    handleSubmit(e) {
-        e.preventDefault();
-
-        alert("501: Form submit not implemented");
-    }
-
-    // handleChange(key, value) {
-    //     this.setState({
-    //         [key]: value
-    //     });
-    // }
-
-
-    render() {
-        return this.state.apiUrl ? (
+  render() {      
+        return (
             <section className="site-section bg-light aos-init aos-animate" id="contact" data-aos="fade">
                 <div className="container">
                     <div className="row mb-5">
@@ -89,53 +30,11 @@ export default class Contact extends React.Component {
                             </p>
                         </div>
                     </div>
-                    <div className="row">
-                        <div className="col-md-12 mb-5">
-                            <form onSubmit={this.handleSubmit} className="p-5 bg-white">
-                                <h2 className="h4 text-black mb-5">Contact Form</h2>
 
-                                <div className="row">
-                                    <FormGroup className="form-group col-12 mb-3 mb-md-0">
-                                        <Input type="name" placeHolder="Name" required
-                                            autoComplete="name" value={this.state.email}
-                                            onChange={this.handleChange.bind(this, 'name')}
-                                        />
-                                    </FormGroup>
-
-                                    <FormGroup className="form-group col-12 mb-3 mb-md-0">
-                                        <Input type="email" placeHolder="Email" required
-                                            autoComplete="email" value={this.state.email}
-                                            onChange={this.handleChange.bind(this, 'email')}
-                                        />
-                                    </FormGroup>
-
-                                    <FormGroup className="form-group col-12 mb-3 mb-md-0">
-                                        <Input type="text" placeHolder="Subject"
-                                            value={this.state.subject}
-                                            onChange={this.handleChange.bind(this, 'subject')}
-                                        />
-                                    </FormGroup>
-
-                                    <FormGroup className="form-group col-12">
-                                        <TextArea placeHolder="Write your notes or questions here..." required
-                                            value={this.state.message} cols="30" rows="7"
-                                            onChange={this.handleChange.bind(this, 'message')}
-                                        />
-                                    </FormGroup>
-
-                                    {/* <Captcha onRef={reCaptcha => (this.reCaptcha = reCaptcha)} theme={'light'} size={'normal'}
-                                        sitekey={this.state.reCaptchaKey} requireReady={false} onVerified={this.onVerified}
-                                    /> */}
-
-                                    <div className="col-md-12">
-                                        <input type="submit" id="submit" value="Send Message" className="btn btn-primary btn-md text-white" />
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
+                    <ContactForm/>
+                    
                 </div>
             </section>
-        ) : null;
+        );
     }
 }
