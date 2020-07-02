069064e32a555e4b44c06749fcbd814d8b802369
MitchellQ <mitchell.quarrie@outlook.com>
2020-07-02 21:25:20 +1200

    Finished upgrade to navbar

--- src/javascript/Components/SiteNav.js
+++ src/javascript/Components/SiteNav.js
@@ -18,8 +18,7 @@ export default class SiteNav extends Component {
         return (
             <Fragment>
                 <HtmlComment text={"Nav"} />
-                {/* Surely a better way to indent than this? */}
-                <nav className="navbar navbar-expand-md stickty-top px-5 mx-5">
+                <nav className="navbar navbar-expand-md stickty-top px-5" >
                     <div className="container-fluid">
                         <h1 className="mb-0 site-logo">
                             <a href="/" className="h2 mb-0">{brand}<span>.</span> </a>
@@ -27,7 +26,7 @@ export default class SiteNav extends Component {
                     </div>
 
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#responsiveNav">
-                        <span><i className="fab fa-bars" /></span>
+                        <span><i className="fab fa-bars menu-icon" /></span>
                     </button>
 
                     <div className="collapse navbar-collapse" style={{ whiteSpace: "nowrap" }} id="responsiveNav">
@@ -35,39 +34,7 @@ export default class SiteNav extends Component {
                             {links}
                         </ul>
                     </div>
-
                 </nav>
-
-                <HtmlComment text={"Main Nav"} />
-                <div id="sticky-wrapper" className="sticky-wrapper" style={{ height: '97px' }}>
-                    <header className="site-navbar py-4 js-sticky-header site-navbar-target" role="banner">
-                        <div className="container">
-                            <div className="row align-items-center">
-                                <HtmlComment text={"Brand"} />
-                                <div className="col-6 col-xl-2">
-                                    <h1 className="mb-0 site-logo">
-                                        <a href="/" className="h2 mb-0">{brand}<span>.</span> </a>
-                                    </h1>
-                                </div>
-
-                                <HtmlComment text={"Nav items"} />
-                                <div className="col-12 col-md-10 d-none d-xl-block">
-                                    <nav className="site-navigation position-relative text-right" role="navigation">
-                                        <ul className="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
-                                            {links}
-                                        </ul>
-                                    </nav>
-                                </div>
-
-                                <HtmlComment text={"Menu Toggle"} />
-                                <div className="col-6 d-inline-block d-xl-none ml-md-0 py-3" style={{ position: 'relative', top: '3px' }}>
-                                    <a href="#" className="site-menu-toggle js-menu-toggle float-right"><span className="icon-menu h3"></span></a>
-                                </div>
-                            </div>
-                        </div>
-                    </header>
-                </div>
-
             </Fragment>
         )
     }
@@ -83,41 +50,10 @@ class NavLink extends Component {
         const { href, name } = this.props;
 
         return <li>
-            <a href={href} className="nav-item js-scroll-trigger d-inline-block px-3" download={name == "Download CV"}>
+            <a href={href} className="js-scroll-trigger px-3" download={name == "Download CV"}>
                 {name == "Download CV" ? <i className="far fa-download pr-2"></i> : null}
                 {name}
             </a>
         </li>
     }
 }
-
-{/*
-                <HtmlComment text={"Main Nav"} />
-                <div id="sticky-wrapper" className="sticky-wrapper" style={{height: '97px'}}>
-                    <header className="site-navbar py-4 js-sticky-header site-navbar-target" role="banner">
-                        <div className="container">
-                            <div className="row align-items-center">
-                                <HtmlComment text={"Brand"} />
-                                <div className="col-6 col-xl-2">
-                                    <h1 className="mb-0 site-logo">
-                                        <a href="/" className="h2 mb-0">{brand}<span>.</span> </a>
-                                    </h1>
-                                </div>
-
-                                <HtmlComment text={"Nav items"} />
-                                <div className="col-12 col-md-10 d-none d-xl-block">
-                                    <nav className="site-navigation position-relative text-right" role="navigation">
-                                        <ul className="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
-                                            {links}
-                                        </ul>
-                                    </nav>
-                                </div>
-
-                                <HtmlComment text={"Menu Toggle"} />
-                                <div className="col-6 d-inline-block d-xl-none ml-md-0 py-3" style={{position: 'relative', top: '3px'}}>
-                                    <a href="#" className="site-menu-toggle js-menu-toggle float-right"><span className="icon-menu h3"></span></a>
-                                </div>
-                            </div>
-                        </div>
-                    </header>
-                </div> */}
