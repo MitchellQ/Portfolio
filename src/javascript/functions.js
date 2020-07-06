8fdd4d2e60643fcc4993f14cc2e7b45938d1aeb7
MitchellQ <mitchell.quarrie@outlook.com>
2020-07-06 19:38:14 +1200

    Add Comments + frmt

--- src/javascript/functions.js
+++ src/javascript/functions.js
@@ -1,38 +1,40 @@
 
-export function  sanitiseHtml(s) {
-    /// <summary>Removes HTML brackets from a string.</summary>
-    /// <param name="s" type="Number">The string to filter.</param>
-    /// <returns type="string">The string without HTML.</returns>
-    return s.replace(/(&nbsp;|<([^>]+)>)/ig, "");
+/// <summary>Removes HTML brackets from a string.</summary>
+/// <param name="s" type="Number">The string to filter.</param>
+/// <returns type="string">The string without HTML.</returns>
+export function sanitiseHtml(s) {
+  return s.replace(/(&nbsp;|<([^>]+)>)/ig, "");
 }
 
-$(function() {
-    $('.js-scroll-trigger').click(function() {
-      var id = $(this).attr('href');
-      $('html,body').animate({
-        scrollTop: $(id).offset().top
-      }, 1000);
 
-      // Prevent default behavior of link
-      return false;
-    });
-  })
+// JS smoothscroll function
+$(function () {
+  $('.js-scroll-trigger').click(function () {
+    var id = $(this).attr('href');
+    $('html,body').animate({
+      scrollTop: $(id).offset().top
+    }, 1000);
 
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
+    // Prevent default behavior of link
+    return false;
   });
+})
 
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#sideNav'
+// Closes responsive menu when a scroll trigger link is clicked
+$('.js-scroll-trigger').click(function () {
+  $('.navbar-collapse').collapse('hide');
+});
+
+// Activate scrollspy to add active class to navbar items on scroll
+$('body').scrollspy({
+  target: '#sideNav'
+});
+
+// Adds scolled class to navbar when scrolled past view height
+$(function () {
+  const navbar = document.querySelector('.navbar');
+
+  window.addEventListener('scroll', () => {
+    window.pageYOffset > $(window).height() ? navbar.classList.add("scrolled") : navbar.classList.remove("scrolled");
   });
-
-
-  $(function() {
-    const navbar = document.querySelector('.navbar');
-  
-    window.addEventListener('scroll', () => {
-      window.pageYOffset > $(window).height() ? navbar.classList.add("scrolled") : navbar.classList.remove("scrolled");
-    });
-  });
+});
