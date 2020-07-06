03a78fd80b8b25f23ecdc7d5af283eaebe3f2229
MitchellQ <mitchell.quarrie@outlook.com>
2020-07-07 08:58:21 +1200

    Update imports + frmt

--- src/javascript/FeaturedProjects.js
+++ src/javascript/FeaturedProjects.js
@@ -1,14 +1,14 @@
 import React from "react";
-import FeaturedProject from "./Components/FeaturedProject";
+import ProjectCard from "./Components/ProjectCard";
 import HtmlComment from './Components/HtmlComment';
 
-export default class FeaturedProjects extends React.Component {
+export default class ProjectCards extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = { 
+		this.state = {
 			projects: require("../data/projects.json"),
 			index: 0
-		} 
+		}
 
 		this.setIndex = this.setIndex.bind(this);
 	}
@@ -16,7 +16,7 @@ export default class FeaturedProjects extends React.Component {
 	// Set an interval to auto play
 	componentDidMount() {
 		this.autoplay = setInterval(() => {
-			this.setIndex(this.state.index +1)
+			this.setIndex(this.state.index + 1)
 		}, 10 * 1000);
 	}
 
@@ -27,53 +27,53 @@ export default class FeaturedProjects extends React.Component {
 
 	// Code to fire each time the index, or projects are updated
 	componentDidUpdate(_, prevState) {
-		if(prevState.projects.length != this.state.projects.length) {
+		if (prevState.projects.length != this.state.projects.length) {
 			this.setIndex(this.state.index);
 		}
 
 		clearInterval(this.autoplay)
 		this.autoplay = setInterval(() => {
-			this.setIndex(this.state.index +1)
+			this.setIndex(this.state.index + 1)
 		}, 10 * 1000);
 	}
 
 	// Adjust the index
 	setIndex(i) {
 		const { projects } = this.state;
-		
-		if(i < 0) {
-			i = projects.length -1
+
+		if (i < 0) {
+			i = projects.length - 1
 		} else if (i >= projects.length) {
 			i = 0;
 		}
-		
-		this.setState({index: i});		 
+
+		this.setState({ index: i });
 	}
 
 	render() {
 		const { index, projects } = this.state;
-		
-		return (
-		<section className="site-section" id="featured">
-			<div className="container">
-				<HtmlComment text="Section mast" />
-				<div className="row mb-5 justify-content-center">
-					<div className="col-md-7 text-center">
-						<h2 className="section-title mb-3" data-aos="fade-up" data-aos-delay="">Featured Projects</h2>
 
-						<p className="lead" data-aos="fade-up" data-aos-delay="100">Dive into the Case Studies of my Featured Projects</p>
+		return (
+			<section className="site-section" id="featured">
+				<div className="container">
+					<HtmlComment text="Section mast" />
+					<div className="row mb-5 justify-content-center">
+						<div className="col-md-7 text-center">
+							<h2 className="section-title mb-3" data-aos="fade-up" data-aos-delay="">Featured Projects</h2>
+
+							<p className="lead" data-aos="fade-up" data-aos-delay="100">Dive into the Case Studies of my Featured Projects</p>
+						</div>
+					</div>
+
+					<HtmlComment text="Featured Project Display" />
+					<div className="featured-project-wrapper" >
+						<ProjectCard project={projects[index]}
+							next={() => this.setIndex(index + 1)}
+							previous={() => this.setIndex(index - 1)}
+						/>
 					</div>
 				</div>
-
-				<HtmlComment text="Featured Project Display" />
-				<div className="featured-project-wrapper" >
-					<FeaturedProject project={projects[index]}
-						next={() => this.setIndex(index +1)}
-						previous={() => this.setIndex(index -1)} 
-					/>
-				</div>
-			</div>
-		</section>
+			</section>
 		);
 	}
 }
