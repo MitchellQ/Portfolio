d3e387f0b0f1b9878631f76c1f7a59a0e66ca2f6
MitchellQ <mitchell.quarrie@outlook.com>
2020-07-06 19:40:30 +1200

    Minor changes
    - Uncomment out code to hide landing etc
    - Remove carousel test import
    - Add function to scroll to section when coming from project specific page

--- src/javascript/Home.js
+++ src/javascript/Home.js
@@ -6,7 +6,6 @@ import FeaturedProjects from "./FeaturedProjects";
 import Testimonials from "./Testimonials";
 import Contact from "./Contact";
 import Education from './Education';
-import Carousel from './Components/Carousel/Carousel'
 
 import Landing from "./Components/Landing";
 
@@ -16,27 +15,41 @@ export default class Home extends React.Component {
 
     this.state = require("../data/siteSettings.json");
 
-    $(function() {
+    // Renders the scroll to top button when Y offset is greater than 100
+    $(function () {
       const scrollUp = document.querySelector('.scrollUp');
-    
+
       window.addEventListener('scroll', () => {
         window.pageYOffset > 100 ? scrollUp.classList.add("active") : scrollUp.classList.remove("active");
       });
     });
   }
 
+  componentDidMount() {
+    // If an #val was specified in the url
+    // scroll there and remove the #val from the window.location
+    let url = window.location;
+    if (url.hash) {
+      $('html,body').animate({
+        scrollTop: $(url.hash).offset().top
+      }, 1000);
+
+      window.history.replaceState({}, document.title,
+        url.toString().substr(0, url.toString().indexOf('#')));
+    }
+  }
+
   render() {
     const { testimonialsVisible, contactFormVisible } = this.state;
-    
+
     return (
       <Fragment>
-        {/* <Landing />
-        <About /> */}
-        <Carousel />
+        <Landing />
+        <About />
         <FeaturedProjects />
         <Education />
         {testimonialsVisible ? <Testimonials /> : null}
-        {contactFormVisible ? <Contact /> : null}        
+        {contactFormVisible ? <Contact /> : null}
 
         <a href="#landing" className="scrollUp js-scroll-trigger"><i className="fa fa-chevron-up"></i></a>
       </Fragment>
