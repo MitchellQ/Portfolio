e770e97b8762de69ba5b976e995fd0046d9c9295
MitchellQ <mitchell.quarrie@outlook.com>
2022-03-28 20:02:34 +1300

    Used Font Awesome layering to replace old social icons

--- src/javascript/Sections/Footer.js
+++ src/javascript/Sections/Footer.js
@@ -20,7 +20,9 @@ export default class Footer extends React.Component {
                 <div className="container">
                     <div className="row text-center">
                         <div className="col-12 social p-3">
-                            {icons}
+                            <div class="fa-3x">
+                                {icons}
+                            </div>
                         </div>
                         <div className="col-12">
                             <div className="border-top pt-3">
