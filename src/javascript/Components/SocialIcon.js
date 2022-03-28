09f34d566c684b2014a4d8e10bb4e72fd10dbb9c
MitchellQ <mitchell.quarrie@outlook.com>
2022-03-28 21:13:08 +1300

    Update SocialIcon.js
    Changed to self closing icon
    Removed extra lines

--- src/javascript/Components/SocialIcon.js
+++ src/javascript/Components/SocialIcon.js
@@ -6,16 +6,12 @@ export default class SocialIcon extends React.Component {
         const { href, icon } = this.props.icon;
 
         return (
-
-
-
             <a href={href} className={`${this.props.className || null} px-2`} target="_blank" rel="noopener">
                 <span className="fa-layers">
-                    <i className="fal fa-circle"></i>
-                    <i className={icon} data-fa-transform="shrink-10"></i>
+                    <i className="fal fa-circle" />
+                    <i className={icon} data-fa-transform="shrink-10" />
                 </span>
             </a>
-
         )
     }
 }
