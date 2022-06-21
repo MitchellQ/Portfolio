eb63f8c7d94722571bc0457f0aa7e3386429d438
MitchellQ <mitchell.quarrie@outlook.com>
2022-06-22 11:02:38 +1200

    Continued to work on Modal

--- src/javascript/Components/Project/Modal.js
+++ src/javascript/Components/Project/Modal.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Modal = props => {
-    if(!props.show) return null;
-    
+
+    const closeOnEscapeKeyDown = (e) => {
+        if((e.charCode || e.keyCode) === 27) {
+            props.onClose()
+        }
+    }
+
+    useEffect(() => {
+        document.body.addEventListener('keydown', closeOnEscapeKeyDown)
+
+        return function cleanup() {
+            document.removeEventListener('keydown', closeOnEscapeKeyDown)
+        }
+    }, [])
+
     return (
-        <div className="-modal">
-            <div className="modal-content">
+        <div className={`modal ${props.show? 'show' : ''}`} onClick={props.onClose}>
+            <div className="modal-content" onClick={e => e.stopPropagation()}>
                 <div className="modal-header">
-                    <h4 className="modal-title">Modal Title</h4>
+                    <h4 className="modal-title">{props.title}</h4>
                 </div>
 
-                <div className="modal-body">
-                    This is modal content
-                </div>
+                <div className="modal-body">{props.children}</div>
 
                 <div className="modal-footer">
-                    <button type="button" className="button">Close</button>
+                    <button onClick={props.onClose} type="button" className="button">Close</button>
                 </div>
             </div>
         </div>
